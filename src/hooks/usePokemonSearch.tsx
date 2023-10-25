b4c942e3978b75c31e8417b29514358b57dc55a8
Jose Luis Zambrano <jzambrano@initiumsoft.com>
2023-10-25 02:57:19 -0500

    main

---
+++ src/hooks/usePokemonSearch.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { pokemonApi } from '../api/pokemonApi';
+import { PokemonPaginatedResponse, SimplePokemon, Result } from '../interfaces/pokemonInterfaces';
+
+export const usePokemonSearch = () => {
+    const [isFetching, setIsFeatching] = useState(true);
+    const [simplePokemonList,setSimplePokemonList] = useState<SimplePokemon[]>([]);
+
+    const loadPokemon = async()=>{
+        const resp = await pokemonApi.get<PokemonPaginatedResponse>('https://pokeapi.co/api/v2/pokemon?limit=1200');
+        mapPokemonList(resp.data.results);
+
+    }
+    const mapPokemonList = (pokemonList: Result[])=>{
+        const newPokemonList: SimplePokemon[] = pokemonList.map(({name,url})=>{
+            const urlParts = url.split('/');
+            const id = urlParts[urlParts.length -2];
+            const picture = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+            return{ id, picture, name }
+        });
+        setSimplePokemonList(newPokemonList);
+        setIsFeatching(false);
+    }
+    
+    useEffect(() => {
+        loadPokemon();
+    }, [])
+    
+    return{
+        isFetching,
+        simplePokemonList,
+    }
+
+}
